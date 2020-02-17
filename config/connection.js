b51563c8b577fb5f5b5a5cee72c00a0dcf59a21f
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-17 16:55:56 -0500

    deploy test

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 const mysql = require("mysql");
-var connection =
-  process.env.JAWSDB_URL ||
-  mysql.createConnection({
+var connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {
     host: "localhost",
     port: 3306,
     user: "huanyang2",
     password: "1234",
     database: "burgers_db"
-  });
+  }
+);
 connection.connect(function(err) {
   if (err) throw err;
 });
