fbf70efa25ec3e88b53239f2aaaa23cb9cf459f5
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-16 21:41:46 -0500

    feat: add model for burger

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,20 @@
 var orm = require("../config/orm");
 // functions to take input and use the input to interact with database
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  add: function(newItem, cb) {
+    orm.add("burgers", "burger_name", newItem, function(res) {
+      cb(res);
+    });
+  },
+  update: function(condition, cb) {
+    orm.update("burgers", "devoured", 1, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 module.exports = burger;
