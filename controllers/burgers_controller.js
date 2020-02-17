6df1e10455aa7475611b93803734f20da46f7dac
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-17 10:32:19 -0500

    feat: enable server and controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,12 @@
 const express = require("express");
 var burger = require("../models/burger");
+var path = require("path");
 // routers
+var router = express.Router();
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    console.log("controller all: " + JSON.stringify(data));
+    res.sendFile(path.join(__dirname, "../test_bench/index.html"));
+  });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const PORT = process.env.PORT || 8080;
+var app = express();
+const morgan = require("morgan");
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// var exphbs = require("express-handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
