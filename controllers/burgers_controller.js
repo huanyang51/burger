752a4cf533c7ccdb8432290e99c5a32b05fe21c5
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-18 19:52:29 -0500

    feat: enable express-handlebars to dynamicly add contents on to the page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,8 +5,30 @@ var path = require("path");
 var router = express.Router();
 router.get("/", function(req, res) {
   burger.all(function(data) {
-    console.log("controller all: " + JSON.stringify(data));
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+router.post("/api/burgers", function(req, res) {
+  console.log("controller add req: " + req.body.burger_name);
+  burger.add(req.body.burger_name, function(result) {
+    console.log("controller add: " + JSON.stringify(result));
     res.sendFile(path.join(__dirname, "../test_bench/index.html"));
+  });
+});
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+  console.log("condition", condition);
+  burger.update(condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// var exphbs = require("express-handlebars");
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
